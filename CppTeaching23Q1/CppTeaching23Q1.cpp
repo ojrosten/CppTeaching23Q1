b299f43f700dba298f7099bed9de91045153e689
Oliver Rosten <oliver.rosten@gmail.com>
2023-03-28 12:55:03 +0100

    Lesson 21

--- CppTeaching23Q1/CppTeaching23Q1.cpp
+++ CppTeaching23Q1/CppTeaching23Q1.cpp
@@ -15,121 +15,8 @@
 #include <functional>
 #include <format>
 #include <sstream>
+#include <variant>
 
-namespace rugby
-{
-    enum class score {
-        rugby_try = 5,
-        conversion = 2,
-        penalty = 3
-    };
-}
-
-enum emotions { love};
-
-
-enum class gl_state { none=0, depth_test=1, stencil_test=2, face_culling=4};
-
-[[nodiscard]]
-gl_state operator|(gl_state lhs, gl_state rhs)
-{
-    return gl_state{static_cast<int>(lhs) | static_cast<int>(rhs)};
-}
-
-
-//
-// 001
-// 010
-// 011
-// 100
-
-// RAII
-// Resource Acquisition Is Initialization
-class wrapper
-{
-public:
-  wrapper() = default;
-
-  wrapper(int x)
-    : m_P{new int{x}}
-  {
-    std::cout << "Constructor\n";
-  }
-
-  wrapper(wrapper const&) = delete;
-
-  /*wrapper(wrapper const& other)
-    : m_P{new int{*(other.m_P)}}
-  {
-    std::cout << "Copy Constructor\n";
-  }*/
-
-  wrapper(wrapper&& other) noexcept
-    : m_P{std::exchange(other.m_P, nullptr)}
-  {
-    std::cout << "Move Constructor\n";
-  }
-
-  wrapper& operator=(wrapper const&) = delete;
-
-  wrapper& operator=(wrapper&& other) noexcept
-  {
-    if(&other != this)
-    {
-      m_P = std::exchange(other.m_P, nullptr);
-    }
-    return *this;
-  }
-
-  ~wrapper()
-  {
-    std::cout << "Destructor\n";
-    delete m_P;
-  }
-
-  [[nodiscard]]
-  int value() const {
-    if(m_P == nullptr)
-      throw std::runtime_error{"null pointer!"};
-
-    return *m_P;
-  }
-
-private:
-  int* m_P{};
-};
-
-wrapper bar(int x) {
-  wrapper w{x};
-  return w;
-}
-
-
-template<class T>
-void swap(T& lhs, T& rhs)
-{
-  auto temp{std::move(lhs)}; // Construction
-  lhs = std::move(rhs);      // assignment
-  rhs = std::move(temp);     // assignment
-}
-
-template<class T>
-constexpr bool has_empty_fn = requires(const T& t){
-  t.empty();
-};
-
-auto pred(int e) -> bool { return e > 60; }
-
-struct moon_pred
-{
-private:
-  int maxMoons{};
-public:
-
-  moon_pred(int m) : maxMoons{m} {};
-
-  bool operator() (int e) const { return e > maxMoons; }
-};
 
 namespace life
 {
@@ -303,6 +190,20 @@ namespace life2
 
     std::unique_ptr<soul> m_pActualAnimal{};
   };
+
+  struct animal_actions
+  {
+    void operator()(const whale& a) const
+    {
+      a.vocalize();
+    }
+
+    void operator()(const dog& a) const
+    {
+      a.vocalize();
+      a.walk();
+    }
+  };
 }
 
 template<class R, class... Args>
@@ -346,17 +247,37 @@ private:
   std::unique_ptr<soul> m_pInvocable;
 };
 
+
+template<class... Ts>
+struct overloaded : Ts...
+{
+  using Ts::operator()...;
+};
+
 int main()
 {
     try
     {
+      std::cout << sizeof(double) << '\n' << sizeof(std::variant<double, float, int>) << '\n';
+
+
       using namespace life2;
 
-      std::vector<animal> v{ whale{}, dog{}, whale{}, whale{} };
+      std::vector<std::variant<whale, dog>> animals{ { whale{} },    { dog{} }     };
+      //                                             ^^ variant^^   ^^variant^^  
+      //                                           ^^
+      //                                          initializing the vector
 
-      for (auto& a : v)
+      for (const auto& a : animals)
       {
-        a.vocalize();
+        std::visit(
+          overloaded{
+            [](const whale& w){ w.vocalize(); },
+            [](const dog& d){ d.walk(); }
+          },
+          a);
+        //std::visit(animal_actions{}, a);
+        //         ^^^ the vistor - defines the action performed for each type 
       }
     }
     catch(const std::out_of_range& e)
